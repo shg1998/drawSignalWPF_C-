43f754b99d399744f681190311e29321e549b3fc
Mohamadhosein NezhadHendi <m.nezhad@saadat.local>
2020-06-07 18:58:01 +0430

    draw signal with sfchart (problem solved)

---
+++ sfchart/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace sfchart
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            Task.Run(new Action(() => {
+
+                while (true)
+                {
+                    Thread.Sleep(400);
+                    this.Dispatcher.Invoke(new Action(() =>
+                    {
+                        this.SSS.ItemsSource = new ViewModel().Data;
+                    }));
+                }
+
+                
+            }));
+        }
+    }
+}
---
+++ sfchart/Sales.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sfchart
+{
+   public class Sales
+    {
+        public string Year { get; set; }
+        public double ProductA { get; set; }
+        public double ProductB { get; set; }
+    }
+}
